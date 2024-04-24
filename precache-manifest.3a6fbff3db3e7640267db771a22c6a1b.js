f87de6f77da67b479e246975d9a0115c9adae3b9
Abebe Adamu <abbkassa@gmail.com>
2024-04-24 03:05:42 -0400

    Updates

---
+++ precache-manifest.3a6fbff3db3e7640267db771a22c6a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f39a852892d5d0a35b67f17ee71558",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7fa8da9e46d7bef18714",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f4d64557f7246b131ae7",
+    "url": "/portfolio/static/js/2.6385edd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.6385edd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa8da9e46d7bef18714",
+    "url": "/portfolio/static/js/main.bd13d264.chunk.js"
+  },
+  {
+    "revision": "f3ebbd0028aa1a714e70",
+    "url": "/portfolio/static/js/runtime-main.3997dff1.js"
+  }
+]);
