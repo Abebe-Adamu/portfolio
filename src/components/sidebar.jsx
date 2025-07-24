c5cd292ecb8e2556ca30c1dc007469dff75625f9
Abebe Adamu <abbkassa@gmail.com>
2025-07-24 11:32:39 -0400

    updated

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -11,7 +11,7 @@ export default class Sidebar extends Component {
                             <div className="author-img" style={{ backgroundImage: 'url(images/about.jpg)' }} />
                             <h1 id="colorlib-logo"><a href="index.html">Abebe Adamu</a></h1>
                             <span className="email"><i className="icon-mail"></i> abbkassa@gmail.com</span>
-                            <span className="number"><i className="icon-phone"></i> 404-563-9988</span>
+                            <span className="number"><i className="icon-phone"></i> 404-307-8602</span>
                         </div>
                         <nav id="colorlib-main-menu" role="navigation" className="navbar">
                             <div id="navbar" className="collapse">
@@ -26,7 +26,7 @@ export default class Sidebar extends Component {
                         </nav>
                         <nav id="colorlib-main-menu">
                             <ul>
-                                <li><a href="https://twitter.com/AbebeAdamu4" target="_blank"><i className="icon-twitter" /></a></li>
+                                <li><a href="https://twitter.com/AbebeAdamu4" target="_blank"><i className="icon-twitter2" /></a></li>
                                 <li><a href="https://www.linkedin.com/in/abebe-adamu-002942164" target="_blank"><i className="icon-linkedin2" /></a></li>
                                 <li><a href="https://github.com/Abebe-Adamu" target="_blank"><i className="icon-github"></i></a></li>
                                 
